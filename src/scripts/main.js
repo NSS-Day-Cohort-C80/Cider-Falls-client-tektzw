053a25d648de903e333048638015557bbb976baf
Kris Cagle <Kriscagle@gmail.com>
2026-03-06 12:02:43 -0600

    Updated main.js added database.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-//test
+import { getGuest, getService, getParkAreaService } from "./data.js";
+
+// guests html generate
+export const guests = () => {
+    let guestHTML = "<ul>"
+    for (const guest of getGuest()) {
+        guestHTML += `<li>${guest.name}</li>` //removed =data-id
+    }
+    guestHTML += "</ul>"
+    return guestHTML
+}
+
+//areas html generate function
+const area (areaId) => {
+    let areaHTML =""
+    
+    }
+}
+
+
+
+
+
+
